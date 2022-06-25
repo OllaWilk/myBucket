26885ccc2e13c0ccd6db5ad0e57bc6e273caa697
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-06-25 14:08:58 +0200

    add delete function to post

--- front/src/components/Post/Post.jsx
+++ front/src/components/Post/Post.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { usePostsContext } from '../../hooks/usePostsContext';
+
 import './Post.scss';
 
 const Post = ({ post }) => {
+  const { dispatch } = usePostsContext();
+
+  const handleClick = async () => {
+    const postID = post._id;
+    const response = await fetch('http://localhost:3000/api/posts/' + postID, {
+      method: 'DELETE',
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_POST', payload: json });
+    }
+  };
+
   return (
     <div className="post">
       <h4>{post.title}</h4>
       <p>{post.createdAt}</p>
+      <span onClick={handleClick}>DELETE</span>
+      {console.log()}
     </div>
   );
 };
